1480ca3790bbb2a21a60e7f2a13356562793417b
Lucca Sanwald <lucca.sanwald@cosee.biz>
2019-05-08 10:55:02 +0200

    * added seperators between links on app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-	<div id="app">
-		<router-link to="/example-preview">Example Preview</router-link>
-		|
-		<router-link to="/async-example-preview">Async Example Preview</router-link>
-		|
-		<router-link to="/lazy-load-example">Lazy Load Example</router-link>
-		|
-		<router-view/>
-	</div>
+  <div id="app">
+    <router-link to="/example-preview">Example Preview</router-link>
+    |
+    <router-link to="/async-example-preview">Async Example Preview</router-link>
+    |
+    <router-link to="/lazy-load-example">Lazy Load Example</router-link>
+    |
+    <router-view />
+  </div>
 </template>
