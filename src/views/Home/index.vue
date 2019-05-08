30bb6bf2ab267823b432ca4d1bfc7e98c60856f6
Lucca Sanwald <lucca.sanwald@cosee.biz>
2019-05-08 14:30:45 +0200

    * styled link list on Home page

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,29 +1,35 @@
 <template>
-  <div>
-    <h2>Project Structure</h2>
-    <router-link to="/example-preview">Example Preview</router-link>
-    |
-    <router-link to="/async-example-preview">Async Example Preview</router-link>
-    |
-    <router-link to="/lazy-load-example">Lazy Load Example</router-link>
-    |
-    <h2>Component Modularity</h2>
-    <router-link to="/directive">Custom Directives</router-link>
-    |
-    <router-link to="/data">Data Loader Component</router-link>
-    |
-    <router-link to="/list">Dynamic List</router-link>
-    |
-    <router-link to="/iscomponent">Component Tag</router-link>
-    |
-    <router-link to="/lifecycle">Lifecycle Hooks</router-link>
-    |
-    <router-link to="/mixin">Mixins</router-link>
-    |
-    <router-link to="/modal">Modal</router-link>
-    |
-    <router-link to="/routing/12/name/somename">Routing</router-link>
-  </div>
+  <section>
+    <article>
+      <h2>Project Structure</h2>
+      <router-link to="/example-preview">Example Preview</router-link>
+      |
+      <router-link to="/async-example-preview"
+        >Async Example Preview</router-link
+      >
+      |
+      <router-link to="/lazy-load-example">Lazy Load Example</router-link>
+      |
+    </article>
+    <article>
+      <h2>Component Modularity</h2>
+      <router-link to="/directive">Custom Directives</router-link>
+      |
+      <router-link to="/data">Data Loader Component</router-link>
+      |
+      <router-link to="/list">Dynamic List</router-link>
+      |
+      <router-link to="/iscomponent">Component Tag</router-link>
+      |
+      <router-link to="/lifecycle">Lifecycle Hooks</router-link>
+      |
+      <router-link to="/mixin">Mixins</router-link>
+      |
+      <router-link to="/modal">Modal</router-link>
+      |
+      <router-link to="/routing/12/name/somename">Routing</router-link>
+    </article>
+  </section>
 </template>
 
 <script>
@@ -32,4 +38,8 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+article {
+  margin-bottom: 2em;
+}
+</style>
