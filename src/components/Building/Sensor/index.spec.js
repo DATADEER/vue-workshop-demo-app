3411f0e10ba30aaed51990dd77d091c1215cb580
Nicklas Knell <nicklas.knell@cosee.biz>
2019-05-10 10:50:15 +0200

    Unit testing

---
+++ src/components/Building/Sensor/index.spec.js
@@ -0,0 +1,50 @@
+import Sensor from ".";
+import { shallowMount } from "@vue/test-utils";
+
+const mockedProps = {
+  room_name: "Bedroom",
+  ip_address: "127.0.0.1",
+  mac_address: "00:0d:83:b1:c0:8e",
+  temperature: 22
+};
+
+describe("Sensor", () => {
+  it("renders without crashing", () => {
+    expect(() => shallowMount(Sensor)).not.toThrow();
+  });
+
+  describe("renders prop", () => {
+    const wrapper = shallowMount(Sensor, { propsData: mockedProps });
+
+    it("room_name", () => {
+      expect(wrapper.find("h3").text()).toContain(mockedProps.room_name);
+    });
+
+    it("ip_address", () => {
+      expect(
+        wrapper
+          .findAll("div")
+          .at(1)
+          .text()
+      ).toContain(mockedProps.ip_address);
+    });
+
+    it("mac_address", () => {
+      expect(
+        wrapper
+          .findAll("div")
+          .at(2)
+          .text()
+      ).toContain(mockedProps.mac_address);
+    });
+
+    it("temperature", () => {
+      expect(
+        wrapper
+          .findAll("div")
+          .at(3)
+          .text()
+      ).toContain(mockedProps.temperature);
+    });
+  });
+});
---
+++ src/components/Building/Sensor/index.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<h3>{{ room_name }}</h3>
+		<div>IP: {{ ip_address }}</div>
+		<div>Mac: {{ mac_address }}</div>
+		<div>Sensor: {{ temperature }}° C</div>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		room_name: String,
+		ip_address: String,
+		mac_address: String,
+		temperature: Number
+	}
+};
+</script>
