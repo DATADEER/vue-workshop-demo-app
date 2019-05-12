d9009dc5939cfae2e68ce0bc675a47bf5fe84bc8
Lucca Sanwald <lucca.sanwald@cosee.biz>
2019-05-12 19:01:57 +0200

    * improved SensorCard component template

--- src/components/Sensors/SensorCard/index.vue
+++ src/components/Sensors/SensorCard/index.vue
@@ -1,10 +1,14 @@
 <template>
-  <v-card
-    data-cy="SensorCard"
-    class="card__container"
-    width="200px"
-    @click="$parent.$emit('removeSensor', sensor)"
-  >
+  <v-card data-cy="SensorCard" class="card__container" width="200px">
+    <div class="card__header">
+      <v-icon
+        @click="$parent.$emit('removeSensor', sensor)"
+        class="material-icons remove__icon"
+        color="white"
+      >
+        close
+      </v-icon>
+    </div>
     <v-card-title primary-title>
       <div>
         <h3 class="headline mb-3">{{ sensor.room_name }}</h3>
@@ -31,8 +35,22 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.card__header {
+  display: flex;
+  justify-content: flex-end;
+}
+
 .card__container {
   display: inline-block;
+}
+
+.remove__icon {
+  background-color: orangered;
+  opacity: 0.2;
+
+  &:hover {
+    opacity: 1;
+  }
 }
 </style>
